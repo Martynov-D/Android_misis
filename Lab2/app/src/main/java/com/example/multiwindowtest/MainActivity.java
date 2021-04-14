bc7bf9a5c9afe16abb1257a534673dc2b9301264
Martynov Dmitry <dimamartynov93@gmail.com>
2021-04-14 16:55:16 +0300

    Сделал лабораторную работу по тесту

--- Lab2/app/src/main/java/com/example/multiwindowtest/MainActivity.java
+++ Lab2/app/src/main/java/com/example/multiwindowtest/MainActivity.java
@@ -8,13 +8,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    public int userScore;
-    public static DataBase db;
+    public int m_userScore;
+    public static DataBase m_db = null;
+    TextView textView;
 
     public final int testRequestCode = 1;
+    public static final String testActivityHash = "testActivityHash";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,27 +25,31 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = new DataBase();
-        userScore = 0;
+        textView = findViewById(R.id.mainTextView);
+
+        m_db = new DataBase();
+        m_userScore = 0;
     }
 
     public void onClick(View view)
     {
-        Intent testIntent = new Intent();
+        Intent testIntent = new Intent(this, TestActivity.class);
         startActivityForResult(testIntent, testRequestCode);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
     {
+        super.onActivityResult(requestCode, resultCode, data);
+
         if (resultCode== Activity.RESULT_CANCELED)
             return;
 
         switch(requestCode)
         {
             case testRequestCode:
+                textView.setText(String.format("User score: %d", data.getIntExtra(testActivityHash, 0)));
                 break;
         }
-        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- Lab2/app/src/main/java/com/example/multiwindowtest/TestActivity.java
+++ Lab2/app/src/main/java/com/example/multiwindowtest/TestActivity.java
@@ -1,8 +1,11 @@
 package com.example.multiwindowtest;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.provider.ContactsContract;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,37 +17,119 @@ import java.util.List;
 public class TestActivity extends AppCompatActivity
 {
     DataBase db;
-    TextView textView;
+    TextView question;
     Button[] buttons;
+    int userScore = 0;
+    int userAnswer = -1;
+    String[] answers=null;
+    int m_index;
+    int nAnswers;
+    Intent infoForMain;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState)
+    public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState, persistentState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        db = MainActivity.db;
-        textView = findViewById(R.id.questionTextView);
+        db = MainActivity.m_db;
+
+        infoForMain = new Intent();
+
+        buttons = new Button[4];
+        question = findViewById(R.id.questionTextView);
         buttons[0] = findViewById(R.id.button1);
         buttons[1] = findViewById(R.id.button2);
         buttons[2] = findViewById(R.id.button3);
         buttons[3] = findViewById(R.id.button4);
+        for (Button button : buttons)
+            button.setEnabled(false);
 
-        runTest();
+        nAnswers = db.size()-1;
+        m_index=0;
+        switchText(m_index);
     }
 
-    void runTest()
+    void switchText(int index)
     {
-        String[] answers;
-        for (int i=0;i<db.size();++i)
+        answers = db.getQuestion(index).getAnswers();
+        question.setText(db.getQuestion(index).getBody());
+        for (int j = 0; j < buttons.length; ++j)
         {
-            answers = db.getQuestion(i).getAnswers();
-            textView.setText(db.getQuestion(i).getBody());
-            for (int j=0;j<buttons.length;++j)
-            {
-                buttons[j].setText(answers[j]);
-            }
-            
+            buttons[j].setText(answers[j]);
+            buttons[j].setEnabled(true);
         }
+    }
+
+    void checkAnswers(int index)
+    {
+        if (userAnswer != db.getQuestion(index).getCorrectAnswerIndex())
+            if(userScore>0)
+                userScore -= 1;
+            else
+                return;
+        else
+            userScore += 2;
+    }
+
+    void sendResult(boolean buttonFlag)
+    {
+        if (buttonFlag)
+        {
+            infoForMain.putExtra(MainActivity.testActivityHash, userScore);
+            setResult(Activity.RESULT_OK, infoForMain);
+            finish();
+        }
+        else
+        {
+            setResult(Activity.RESULT_CANCELED, infoForMain);
+            finish();
+        }
+    }
+
+    public void b1onClick(View view)
+    {
+        userAnswer = 1;
+        checkAnswers(m_index);
+        if (m_index<nAnswers)
+            switchText(++m_index);
+        else
+            sendResult(true);
+    }
+
+    public void b2onClick(View view)
+    {
+        userAnswer = 2;
+        checkAnswers(m_index);
+        if (m_index<nAnswers)
+            switchText(++m_index);
+        else
+            sendResult(true);
+
+    }
+
+    public void b3onClick(View view)
+    {
+        userAnswer = 3;
+        checkAnswers(m_index);
+        if (m_index<nAnswers)
+            switchText(++m_index);
+        else
+            sendResult(true);
+    }
+
+    public void b4onClick(View view)
+    {
+        userAnswer = 4;
+        checkAnswers(m_index);
+        if (m_index<nAnswers)
+            switchText(++m_index);
+        else
+            sendResult(true);
+    }
+
+    public void backButtononClick(View view)
+    {
+        sendResult(false);
     }
 }
